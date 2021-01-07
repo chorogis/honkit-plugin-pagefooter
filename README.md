43743a755de45210df67d44f4b5c9ffd0893a435
cho <hayashi@cyberdefense.jp>
2021-01-07 13:36:42 +0900

    init

---
+++ README.md
@@ -0,0 +1,20 @@
+# honkit-plugin-pagefooter
+
+## Usage
+
+```json
+"plugins":{
+  "page-footer"
+}
+```
+
+## Configuration
+
+```javascript
+"page-footer": {
+  "footer":"strings are here",
+  "label": "Last modified: ",
+  "format": "YYYY-MM-DD HH:mm:ss",
+  "utc" : true,
+}
+```
---
+++ index.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const moment = require('moment')
+let config
+
+module.exports = {
+  book: {
+    assets: './assets',
+    css: [
+      'footer.css'
+    ],
+  },
+  hooks: {
+    'page:before': function (page) {
+      config = this.config.get("pluginsConfig.page-footer")
+
+      if(config === undefined) return page
+
+      const label = config.label || "Modified: "
+      const datefmt = config.format || "YYYY-MM-DD HH:mm:ss"
+      const footer = '<footer class="page-footer">' + config.footer +
+        '<span class="footer-modification">' +
+        label + '\n{{file.mtime | date("' + datefmt + '")}}\n</span></footer>'
+
+      console.log(footer)
+
+      page.content = page.content + footer;
+      return page;
+    }
+  },
+  filters: {
+    date: function (d, format) {
+      const isUTC = config.utc === true || false
+      if (isUTC) {
+        return moment(d).utc().format(format)
+      } else {
+        return moment(d).local().format(format)
+      }
+    }
+  }
+};
